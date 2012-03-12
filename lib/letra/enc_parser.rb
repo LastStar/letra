dfe3a2bce6f74cdde9adbcd87a494691f907c87e
Josef Šimánek <retro@ballgag.cz>
2012-03-12 13:25:17 +0100

    Empty branch

--- lib/letra/enc_parser.rb
+++
@@ -1,7 +0,0 @@
-class Letra
-  class EncParser
-    def self.parse(input)
-      return input.split("\n").collect {|line| line.split(" ").last.to_i }
-    end
-  end
-end
--- test/spec_helper.rb
+++
@@ -1,3 +0,0 @@
-$: << File.expand_path('..', File.dirname(__FILE__))
-require 'minitest/autorun'
-require "lib/letra"
