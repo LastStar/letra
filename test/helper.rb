781736514e99f88f3880d8607760c9be25c7bf3d
Josef Šimánek <retro@ballgag.cz>
2012-04-09 19:27:54 +0200

    Better test for subs with glyphr

--- test/helper.rb
+++ test/helper.rb
@@ -10,20 +10,17 @@ require 'glyphr'
 class Letra::TestCase < MiniTest::Unit::TestCase
   attr_accessor :font, :letra, :dir
   def setup
-    self.dir   = Dir.mktmpdir('letra')
-    self.letra = Letra.new('test/fonts/font.otf')
-    self.letra.destination = dir
+    self.dir = Dir.mktmpdir('letra')
   end
 
   def teardown
-    self.letra.close
     FileUtils.remove_entry_secure self.dir
   end
 
   private
 
-  def generated_font_path(extension)
-    filepath = File.join(dir, "#{letra.name}.#{extension}")
+  def generated_font_path(name, extension)
+    filepath = File.join(dir, "#{name}.#{extension}")
   end
 
   def assert_generated_file_format(extension, test_with_fontforge = true)
@@ -36,4 +33,18 @@ class Letra::TestCase < MiniTest::Unit::TestCase
     filepath = generated_font_path(extension)
     assert !File.exists?(filepath), "#{filepath} exists, but should not!"
   end
+
+ def assert_rendered_text(font, text, image)
+    renderer = Glyphr::Renderer.new(font, 72)
+    renderer.image_width = 150
+    renderer.render(text)
+    generated_image = File.join(self.dir, 'output.png')
+    renderer.image.save(generated_image)
+    assert_files(generated_image, image)
+  end
+
+ def assert_files(file1, file2)
+   assert FileUtils.compare_file(file1, file2), "Files #{file1} and #{file2} are not same."
+ end
+
 end
---
+++ test/test_letra_ubuntu_lookups.rb
@@ -0,0 +1,37 @@
+require 'helper'
+
+class TestLetraUbuntuLookups < Letra::TestCase
+  attr_accessor :font, :letra, :dir
+  def setup
+    self.dir = Dir.mktmpdir('letra')
+  end
+
+  def teardown
+    FileUtils.remove_entry_secure self.dir
+  end
+
+
+  def test_apply_substitution
+    Letra.open('test/fonts/ubuntu.otf') do |font|
+      font.destination = self.dir
+      font.apply_substitutions = ['case']
+      font.formats = [:otf]
+      font.name = 'superfont'
+    end
+
+    assert_rendered_text(generated_font_path('superfont', 'otf'),
+                         'abcd', 'test/images/ubuntu_case.png')
+  end
+
+  def test_multiple_substitutions
+    Letra.open('test/fonts/ubuntu.otf') do |font|
+      font.destination = self.dir
+      font.apply_substitutions = ['case', 'numr']
+      font.formats = [:otf]
+      font.name = 'superfont'
+    end
+
+    assert_rendered_text(generated_font_path('superfont', 'otf'),
+                         'ab12', 'test/images/ubuntu_case_numr.png')
+  end
+end
