255acbab57449e57bded4afdcc58f77386d6f74c
Josef Šimánek <retro@ballgag.cz>
2012-04-09 19:14:06 +0200

    Another test for not required arguments

--- test/test_letra_block_syntax.rb
+++ test/test_letra_block_syntax.rb
@@ -10,7 +10,7 @@ class TestLetraBlockSyntax < Letra::TestCase
     FileUtils.remove_entry_secure self.dir
   end
 
-  def test_letra_block_syntax
+  def test_letra_block_syntax_with_all
     Letra.open('test/fonts/font.otf') do |font|
       font.destination = self.dir
       font.remove_kerning = true
@@ -23,5 +23,17 @@ class TestLetraBlockSyntax < Letra::TestCase
     assert File.exists?(File.join(self.dir, 'superfont.woff'))
     assert File.exists?(File.join(self.dir, 'superfont.eot'))
     refute File.exists?(File.join(self.dir, 'superfont.ttf'))
+  end
+
+  def test_letra_block_syntax_with_required
+    Letra.open('test/fonts/font.otf') do |font|
+      font.destination = self.dir
+      font.formats = [:woff, :eot, :ttf]
+      font.name = 'superfont'
+    end
+
+    assert File.exists?(File.join(self.dir, 'superfont.woff'))
+    assert File.exists?(File.join(self.dir, 'superfont.eot'))
+    assert File.exists?(File.join(self.dir, 'superfont.ttf'))
   end
 end
